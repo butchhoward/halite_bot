ba9bd0a9eb4ac90e018bea9cd6dbc8ad054c4134
Butch Howard <butchhoward@gmail.com>
2017-11-02 14:28:41 -0400

    Run game variations; capture the key output

--- run_game.sh
+++ run_game.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
-
 set -e
 
-../halite -d "240 160" "./MyBot" "./MyBot"
+rm *.hlt
+rm t.out
+echo "=====" > t.out
+../halite -d "240 160" "./MyBot" "./MyBot" | tee /dev/tty | grep -v '^Turn' >> t.out
+
+echo "=====" >> t.out
+../halite -d "264 176" "./MyBot" "./MyBot" | tee /dev/tty | grep -v '^Turn' >> t.out
+
+echo "=====" >> t.out
+../halite -d "360 240" "./MyBot" "./MyBot" | tee /dev/tty | grep -v '^Turn' >> t.out
+
+echo "=====" >> t.out
+../halite -d "240 160" "./MyBot" "./MyBot" "./MyBot" "./MyBot" | tee /dev/tty | grep -v '^Turn' >> t.out
+
+echo "=====" >> t.out
+../halite -d "384 256" "./MyBot" "./MyBot" "./MyBot" "./MyBot" | tee /dev/tty | grep -v '^Turn' >> t.out
+
+echo "=====" >> t.out
+../halite -d "288 192" "./MyBot" "./MyBot" "./MyBot" "./MyBot" | tee /dev/tty | grep -v '^Turn' >> t.out
