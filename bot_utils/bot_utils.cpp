02b2a87cfb8374fceb42d54c6d74561be16aa30f
Butch Howard <butchhoward@gmail.com>
2017-11-02 00:56:12 -0400

    Remove unused code

--- bot_utils/bot_utils.cpp
+++ bot_utils/bot_utils.cpp
@@ -1,8 +1,4 @@
 #include "bot_utils.hpp"
 #include "log_utils.hpp"
 
-bool planet_less_than_halffull(const hlt::Planet &planet)
-{
-    return planet.docked_ships.size() < (planet.docking_spots / 2);
-}
 
--- bot_utils/bot_utils.hpp
+++ bot_utils/bot_utils.hpp
@@ -2,5 +2,3 @@
 
 #include "hlt/hlt.hpp"
 
-bool planet_less_than_halffull(const hlt::Planet &planet);
-hlt::possibly<hlt::Move> attack_something(const hlt::Map map, const hlt::Planet &planet, const hlt::Ship &ship);
