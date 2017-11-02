548581b191b7ca6645fd419854fc2c1422bf9ac9
Butch Howard <butchhoward@gmail.com>
2017-11-02 16:33:21 -0400

    Add timestamp to log file name
    to sort of connect it to the replay file name

--- hlt/hlt.hpp
+++ hlt/hlt.hpp
@@ -1,10 +1,23 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+#include <ctime>
 
 #include "log.hpp"
 #include "hlt_in.hpp"
 #include "hlt_out.hpp"
+
+std::string xput_time() 
+{
+    // While compilers like G++4.8 report C++11 compatibility, they do not
+    // support std::put_time, so we have to use strftime instead.
+    auto time = std::time(nullptr);
+    auto localtime = std::localtime(&time);
+    char result[30];
+    std::strftime(result, 30, "%Y%m%d-%H%M%S%z-", localtime);
+    return std::string(result);
+}
 
 namespace hlt {
     struct Metadata {
@@ -27,7 +40,12 @@ namespace hlt {
         int map_height;
         iss2 >> map_width >> map_height;
 
-        Log::open(std::to_string(player_id) + "_" + bot_name + ".log");
+        std::stringstream log_filename;
+        log_filename    << player_id 
+                        << "_" << bot_name 
+                        << "_" << xput_time() 
+                        <<  ".log";
+        Log::open(log_filename.str());
 
         std::stringstream bot_name_with_decoration;
         bot_name_with_decoration << bot_name << "_" << player_id;
