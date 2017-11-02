5e61d861b9496aeed75f7f9830f74dcbb9a2c3ce
Butch Howard <butchhoward@gmail.com>
2017-11-02 16:33:45 -0400

    Use Replays folder for output

--- run_game.sh
+++ run_game.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-rm *.hlt
-rm t.out
 echo "=====" > t.out
 ../halite -d "240 160" "./MyBot" "./MyBot" | tee /dev/tty | grep -v '^Turn' >> t.out
 
@@ -20,3 +18,5 @@ echo "=====" >> t.out
 
 echo "=====" >> t.out
 ../halite -d "288 192" "./MyBot" "./MyBot" "./MyBot" "./MyBot" | tee /dev/tty | grep -v '^Turn' >> t.out
+
+mv *.log ./Replays/
