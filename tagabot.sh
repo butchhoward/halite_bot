a4a4d4f4a80a79daa2049f229f369260aa2269cc
Butch Howard <butchhoward@gmail.com>
2017-11-02 23:15:55 -0400

    Tagging and zipping. try to move to out-of-source build

--- tagabot.sh
+++ tagabot.sh
@@ -19,13 +19,11 @@ function repo_is_clean()
     return 0
 }
 
-if ! repo_is_clean ; then
-    echo "git repo has uncommited changes pending"
-    exit 1
-fi
 
-HASH=$(git rev-parse --short HEAD)
-BOT_NAME="MyBot_${HASH}"
+# if ! repo_is_clean ; then
+#     echo "git repo has uncommited changes pending"
+#     exit 1
+# fi
 
 ./buildabot.sh
 if [ ! $? ]; then
@@ -33,6 +31,37 @@ if [ ! $? ]; then
     exit 2
 fi
 
-mv MyBot "${BOT_NAME}"
+GIT_COMMIT=`git rev-parse HEAD`
+NEEDS_TAG=`git describe --contains ${GIT_COMMIT} 2>/dev/null`
+
+#only tag if no tag already (would be better if the git describe command above could have a silent option)
+if [ -z "${NEEDS_TAG}" ]; then
+    #get highest tag number
+    VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
+
+    #replace . with space so can split into an array
+    VERSION_BITS=(${VERSION//./ })
+
+    #get number parts and increase last one by 1
+    VNUM1=${VERSION_BITS[0]}
+    VNUM2=${VERSION_BITS[1]}
+    VNUM3=${VERSION_BITS[2]}
+    VNUM3=$((VNUM3+1))
+
+    #create new tag
+    NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
+
+    echo "Updating ${VERSION} to ${NEW_TAG}"
+
+    echo "Tagged with ${NEW_TAG}"
+    git tag ${NEW_TAG}
+    #git push --tags
+
+    BOT_NAME="MyBot_${NEW_TAG}"    
+    mv MyBot "${BOT_NAME}"
+else
+    echo "Already a tag on this commit"
+fi
+
 echo "finished"
 
--- zipbot.sh
+++ zipbot.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 rm halite_bot.zip &> /dev/null
-zip -r halite_bot.zip ./hlt *.cpp *.hpp MyBot.cpp
+zip -r bot.zip ./hlt ./bot MyBot.cpp
